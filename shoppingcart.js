fbcbba47241144606232b992898b4098711e6cb3
Heisenberg80M <58119436+Heisenberg80M@users.noreply.github.com>
2019-12-02 07:26:14 +0200

    totals adds
    -calculation of totals added

--- shoppingcart.js
+++ shoppingcart.js
@@ -2,10 +2,11 @@
 /*-- Tafadzwa Nyazenga-->*/
 /*-- 23/11/2019 -->*/
 
-var  items = 0;
+var items = 0;
 var itemsInCart = 0;
 var buttonsInCart = "";
 let colorclasses;
+let total;
 
 
 // Color class with the associated attributes and methods (getters and setters)
@@ -23,7 +24,7 @@ class Color {
         return this.quantity;
     }
     get colorPriceGet() {
-        return this.colorPrice + "&nbsp;&nbsp;&nbsp;";
+        return this.colorPrice;
     }
 
     get colorFullPriceGet() {
@@ -31,15 +32,17 @@ class Color {
     }
 
     get button() {
-        var addedItem = document.createElement("Button"); //Create a button element
-        var addedItemColor = "button-" + colorID.innerHTML.toLowerCase().replace(" ", "-"); //fetch button class
+        let btnName = this.colorID.toLowerCase().replace(" ","-");
+        return "<button class='inline-block color-button button-" + btnName + "' onclick=colorSelect('" + this.colorID + "');></button>";
+        //var addedItem = document.createElement("Button"); //Create a button element
+        //var addedItemColor = "button-" + colorID.innerHTML.toLowerCase().replace(" ", "-"); //fetch button class
 
-        //add relevant classes to button
+        /*add relevant classes to button
         addedItem.classList.add("inline-block"); 
         addedItem.classList.add("color-button");
         addedItem.classList.add(addedItemColor);
     
-        return addedItem;
+        return addedItem;*/
     }
 }
 
@@ -61,7 +64,7 @@ function createColorClasses(){
                 "Pink": new Color(17.95, "Pink"),
                 "Cream": new Color(14.95, "Cream"),
                 "Emerald Green": new Color(16.95, "Emerald Green"),
-                "Dark Grey": new Color(19.00, "Dark Grey"),
+                "Dark Grey": new Color(19.45, "Dark Grey"),
                 "Brown": new Color(11.95, "Dark Grey"),
                 "Aqua Marine": new Color(9.95, "Aqua Marine"),
                 "Light Grey": new Color(12.95, "Light Grey"),
@@ -91,7 +94,7 @@ function agree() {
     /*--Add requested items to cart--*/
     if(document.getElementById("colorModal")!="Please choose a color"){
 
-        document.getElementById("quantity").innerHTML = itemsInCart;
+        
         document.getElementById("addToCart").innerHTML = "Check out Now"; //only update if colour has been selected
         document.querySelector("#addToCart").dataset.target = "#myCheckoutModal";
         
@@ -100,22 +103,45 @@ function agree() {
         //document.getElementById("selections").appendChild(colorclasses[c].button);
 
         var i = 0;
-        for (i = 0; i < items; i++) {
+        total = 0;
+        var dicountedTotal = 0;
+        for (const a of colors) {
+            if(colorclasses[a].quantityGet!=0){
+                for(i=1; i<=colorclasses[a].quantityGet; i++){
             
-            var addedItem = document.createElement("Button"); //Create a button element
-            var addedItemColor = "button-" + document.getElementById("color").innerHTML.toLowerCase().replace(" ", "-"); //fetch button class
+                    //var addedItem1 = colorclasses[item].button; //Create a button element
+                    /*var addedItemColor = "button-" + document.getElementById("color").innerHTML.toLowerCase().replace(" ", "-"); //fetch button class
 
-            //add relevant classes to button
-            addedItem.classList.add("inline-block"); 
-            addedItem.classList.add("color-button");
-            addedItem.classList.add(addedItemColor);
+                    //add relevant classes to button
+                    addedItem.classList.add("inline-block"); 
+                    addedItem.classList.add("color-button");
+                    addedItem.classList.add(addedItemColor);*/
 
-            
-            document.getElementById("selections").appendChild(addedItem);
+                    
+                    //document.getElementById("selections").appendChild(addedItem1);
+
+                    buttonsInCart += colorclasses[a].button;
+                    dicountedTotal += colorclasses[a].colorPriceGet;
+                    total += colorclasses[a].colorFullPriceGet;
+                }
+            }
         }
     
     }
     /*-----------------------------------------------------------------------*/
+    
+    
+    document.querySelector("#selections").innerHTML=buttonsInCart;
+    buttonsInCart ="";
+
+    var element = document.getElementById("selections");
+    var numberOfChildren = element.getElementsByTagName('*').length;
+
+    document.getElementById("quantity").innerHTML = numberOfChildren;
+    document.querySelector("#items").innerText = "0";
+
+    document.getElementById("price").innerHTML = "$"+dicountedTotal.toFixed(2);
+    document.getElementById("full-price").innerHTML = "$"+(dicountedTotal*1.25).toFixed(2);
 
     items = 0;
     modal.style.display = "none";
@@ -123,8 +149,17 @@ function agree() {
 }
 
 function updateCart(){
-    document.querySelector("items").innerText = "0";
-    document.getElementById("items").innerHTML = "0";
+
+    if(document.getElementById("colorModal")!="Please choose a color"){
+        document.getElementById("items").innerHTML = "0";
+    }
+    else{
+        document.getElementById("items").innerHTML = colorclasses[document.getElementById("colorModal")].quantityGet;
+        document.getElementById("selections").appendChild(document.getElementById("colorModal"));
+    }
+    //let c = document.getElementById("colorModal").innerHTML;
+    
+    //document.getElementById("items").innerHTML = "0";
 }
 
 
